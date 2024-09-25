0791db9c0bf909421a55c577f7f4e991cfecc28a
fageer <fageern@gmail.com>
2024-09-25 06:17:36 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,44 @@
+import { Link } from 'react-router-dom';
+import footerLogo from "../assets/images/footerlogo.png"
 function Footer(){
     return (
         <footer>
-            
+            <div className='footer'>
+                <div>
+                    <img src={footerLogo} alt="logo" />
+                </div>
+                <li className='footer_li'>
+                <h1>Navigation</h1>
+                    <ul className='footer-nav'>
+                        <li><Link className='link' to="/">Home</Link></li>
+                        <li><Link className='link' to="/about">About</Link></li>
+                        <li><Link className='link' to="/menu">Menu</Link></li>
+                        <li><Link className='link' to="/reservations">Reservations</Link></li>
+                        <li><Link className='link' to="/order">Order online</Link></li>
+                        <li><Link className='link' to="/login">Login</Link></li>
+                    </ul>
+                </li>
+                <li className='footer_li'>
+                <h1>Contact</h1>
+                    <ul className='footer-nav'>
+                        <li>2395 Maldove Way,</li>
+                        <li>Riyadh</li>
+                        <li>(629)-243-6827</li>
+                        <li>info@littlelemon.com</li>
+                    </ul>
+                </li>
+                <li className='footer_li'>
+                <h1>Connect</h1>
+                    <ul className='footer-nav'>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Join Us!</li>
+                    </ul>
+                </li>
+            </div>
+            <div>
+                <p>�� 2022 Little Lemon. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import highlight1 from "../assets/images/greek salad.jpg"
-import highlight2 from "../assets/images/1_ji5jV-KoRbEFScZQUPYyGg.png"
+import highlight2 from "../assets/images/food3.jpg"
 import highlight3 from "../assets/images/lemon dessert.jpg"
 
 function Highlight() {
